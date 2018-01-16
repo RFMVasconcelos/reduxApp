c16b24ae882c64cf422b8232469bdbb4c59621d0
RFMVasconcelos <rui.vasconcelos.mail@gmail.com>
2018-01-16 21:26:30 +0100

    footer added

--- public/bundle.js
+++ public/bundle.js
@@ -19464,6 +19464,10 @@ var _menu = __webpack_require__(502);
 
 var _menu2 = _interopRequireDefault(_menu);
 
+var _footer = __webpack_require__(503);
+
+var _footer2 = _interopRequireDefault(_footer);
+
 var _cartActions = __webpack_require__(136);
 
 var _booksActions = __webpack_require__(108);
@@ -19501,7 +19505,8 @@ var store = (0, _redux.createStore)(_index2.default, middleware);
     'div',
     null,
     _react2.default.createElement(_menu2.default, null),
-    _react2.default.createElement(_booksList2.default, null)
+    _react2.default.createElement(_booksList2.default, null),
+    _react2.default.createElement(_footer2.default, null)
   )
 ), document.getElementById('app'));
 
@@ -45104,7 +45109,7 @@ var Menu = function (_React$Component) {
     value: function render() {
       return _react2.default.createElement(
         _reactBootstrap.Navbar,
-        { inverse: true, fixedTop: true },
+        { inverse: true, collapseOnSelect: true },
         _react2.default.createElement(
           _reactBootstrap.Navbar.Header,
           null,
@@ -45164,6 +45169,64 @@ var Menu = function (_React$Component) {
 }(_react2.default.Component);
 
 exports.default = Menu;
+
+/***/ }),
+/* 503 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Footer = function (_React$Component) {
+  _inherits(Footer, _React$Component);
+
+  function Footer() {
+    _classCallCheck(this, Footer);
+
+    return _possibleConstructorReturn(this, (Footer.__proto__ || Object.getPrototypeOf(Footer)).apply(this, arguments));
+  }
+
+  _createClass(Footer, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'footer',
+        { className: 'footer text-center' },
+        _react2.default.createElement(
+          'div',
+          { className: 'container' },
+          _react2.default.createElement(
+            'p',
+            { className: 'footer-text' },
+            ' This course is not as easy as I thought'
+          )
+        )
+      );
+    }
+  }]);
+
+  return Footer;
+}(_react2.default.Component);
+
+exports.default = Footer;
 
 /***/ })
 /******/ ]);
